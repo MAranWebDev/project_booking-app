a98ba65da60011a991421086219cc8e7a677ed34
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-11-25 19:00:08 -0300

    chore: more setups

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,21 +1,8 @@
 // Direct imports
-import './globals.css';
+import '@/assets/styles/globals.css';
 
 // Libs
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-
-// Fonts
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -29,11 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
