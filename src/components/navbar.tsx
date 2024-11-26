e17f70dfe6659dfa0bf2359c805bf2e5e81b78b2
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-11-26 18:03:17 -0300

    navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,31 @@
+import styles from '@/assets/styles/navbar.module.css'; // Asumiendo que usarás módulos de CSS para estilos
+import Link from 'next/link';
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li>
+          <Link href="/Home" legacyBehavior>
+            <a>Inicio</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about" legacyBehavior>
+            <a>Quiénes Somos</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" legacyBehavior>
+            <a>Contacto</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/signup" legacyBehavior>
+            <a>Regístrate</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
