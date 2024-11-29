87584cef286b035b83065f070389df5be8e5424c
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-11-29 16:56:07 -0300

    minor fix in zod schemas

--- src/libs/zod/schemas/users.zod.ts
+++ src/libs/zod/schemas/users.zod.ts
@@ -6,7 +6,9 @@ const email = z.string().trim().email().min(5).max(20);
 const password = z.string().min(8).max(20);
 
 // Exported schemas
-export const usersZodCreateInput = z.object({ name, email, password });
+export const usersZodSignup = z.object({ name, email, password });
+export const usersZodSignin = z.object({ email, password });
 
 // Exported types
-export type UsersZodCreateInput = z.infer<typeof usersZodCreateInput>;
+export type UsersZodSignup = z.infer<typeof usersZodSignup>;
+export type UsersZodSignin = z.infer<typeof usersZodSignin>;
