812517426f9197597d998750520a70dec8d95167
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-11-29 15:58:30 -0300

    minor fixes

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,2 +1,3 @@
 // Server
 export const MONGODB_URI = process.env.MONGODB_URI as string;
+export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET as string;
--- src/libs/mongoose/db-connect.ts
+++ src/libs/mongoose/db-connect.ts
@@ -1,13 +1,18 @@
 import { MONGODB_URI } from '@/config/env';
 import mongoose from 'mongoose';
 
+if (!MONGODB_URI) throw new Error('MONGODB_URI must be defined');
+
 export const dbConnect = async () => {
   try {
     // Establish the database connection
     await mongoose.connect(MONGODB_URI);
 
     // Check if there is only one connection
-    if (mongoose.connection.readyState === 1) console.log('MongoDB Connected');
+    if (mongoose.connection.readyState === 1) {
+      console.log('MongoDB Connected');
+      return true;
+    }
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     throw error;
