395336116b77af1518121612d3623af5f4fe6a48
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-11-29 17:52:41 -0300

    added route

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,50 @@
+import { dbConnect } from '@/libs/mongoose/db-connect';
+import { User } from '@/libs/mongoose/models';
+import { usersZodSignin } from '@/libs/zod/schemas';
+import bcrypt from 'bcryptjs';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+// Types
+interface CustomUser {
+  _id: string;
+  email: string;
+  name: string;
+}
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        await dbConnect();
+
+        // Parse and validate request body
+        const { data } = usersZodSignin.safeParse(credentials);
+        const { email, password } = data!;
+        const user = await User.findOne({ email }).select('+password');
+        const doesPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!user && !doesPasswordMatch) throw new Error('Invalid credentials');
+        return user;
+      },
+    }),
+  ],
+  pages: { signIn: '/login' },
+  session: { strategy: 'jwt' },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user as CustomUser;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
--- src/libs/mongoose/models/user.ts
+++ src/libs/mongoose/models/user.ts
@@ -9,4 +9,5 @@ const UserSchema = new Schema(
   { timestamps: true },
 );
 
+// Export the model, ensuring it's typed correctly
 export const User = models.User || mongoose.model('User', UserSchema);
