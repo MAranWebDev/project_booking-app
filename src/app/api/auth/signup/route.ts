5260fa2c1ed5a7ad6844c13a6250246ce56e75dc
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-11-29 17:25:09 -0300

    added signup route

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,40 +1,45 @@
 import { dbConnect } from '@/libs/mongoose/db-connect';
 import { User } from '@/libs/mongoose/models';
+import { usersZodSignup } from '@/libs/zod/schemas';
 import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   try {
+    // Parse and validate request body
+    const body = await req.json();
+    const { data } = usersZodSignup.safeParse(body);
+    const { name, email, password } = data!;
+
     // Database connection
     await dbConnect();
 
-    const { name, email, password } = await req.json();
-
     // Check if user already exists
-    const userFound = await User.findOne({ email });
-
+    const userFound = await User.exists({ email });
     if (userFound)
       return NextResponse.json(
         { message: 'Email already exists' },
         { status: 409 },
       );
 
+    // Hash password and create user
     const hashedPassword = await bcrypt.hash(password, 12);
-
     const { createdAt, updatedAt } = await User.create({
       name,
       email,
       password: hashedPassword,
     });
-
     return NextResponse.json(
       { name, email, createdAt, updatedAt },
       { status: 201 },
     );
   } catch (error) {
-    if (error instanceof mongoose.Error.ValidationError)
-      return NextResponse.json({ message: error.message }, { status: 400 });
-    return NextResponse.error();
+    const status = error instanceof mongoose.Error.ValidationError ? 400 : 500;
+    const message =
+      error instanceof mongoose.Error.ValidationError
+        ? error.message
+        : 'Internal Server Error';
+    return NextResponse.json({ message }, { status });
   }
 }
