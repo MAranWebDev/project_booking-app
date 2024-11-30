2daa7c78ed5bb042466250e49b886e5d2efd8024
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-11-30 10:53:10 -0300

    test

--- src/components/ui/dropdowns/language-dropdown.tsx
+++ src/components/ui/dropdowns/language-dropdown.tsx
@@ -3,7 +3,6 @@
 import { Locale, LOCALES } from '@/libs/next-intl/constants';
 import { setUserLocale } from '@/libs/next-intl/utils';
 import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { useLocale, useTranslations } from 'next-intl';
@@ -22,15 +21,8 @@ export const LanguageDropdown = () => {
     setUserLocale(event.target.value as Locale);
 
   return (
-    <FormControl>
-      <InputLabel
-        id="select-label"
-        sx={{ color: 'inherit', '&.Mui-focused': { color: 'inherit' } }}
-      >
-        {t('label')}
-      </InputLabel>
+    <FormControl variant="standard">
       <Select
-        labelId="select-label"
         id="select"
         value={currentLocale}
         onChange={handleChange}
