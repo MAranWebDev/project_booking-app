da240c98ad2a13482a072fadc2c36bcc4d565449
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-02 17:33:58 -0300

    navbr

--- src/components/ui/headers/navbar.tsx
+++ src/components/ui/headers/navbar.tsx
@@ -2,52 +2,35 @@ import { LanguageDropdown } from '@/components/ui/dropdowns';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { getServerSession } from 'next-auth';
 import { getTranslations } from 'next-intl/server';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const Navbar = async () => {
-  const session = await getServerSession();
   const t = await getTranslations('Navbar');
 
   return (
-    <AppBar position="static">
-      <Toolbar
-        sx={{
-          justifyContent: 'space-between',
-          width: '90%',
-          mx: 'auto',
-          my: 1,
-        }}
-      >
-        <Link href="/">
-          <Typography variant="h6" component="div">
-            Next Auth
-          </Typography>
-        </Link>
+    <header>
+      <AppBar position="fixed">
+        <Toolbar sx={{ width: '90%', mx: 'auto', gap: 2 }}>
+          <Link href="/">
+            <Image src="/images/logo.png" alt="Logo" width={60} height={60} />
+          </Link>
 
-        <Box
-          sx={{
-            display: 'flex',
-            gap: 2,
-            justifyContent: 'end',
-            alignItems: 'center',
-          }}
-        >
-          <Link href="/">{t('home')}</Link>
+          <Box sx={{ flexGrow: 1 }} />
 
-          {session ? (
-            <Link href="/dashboard/profile">{t('profile')}</Link>
-          ) : (
-            <>
-              <Link href="/signup">{t('register')}</Link>
-              <Link href="/signin">{t('login')}</Link>
-            </>
-          )}
-          <LanguageDropdown />
-        </Box>
-      </Toolbar>
-    </AppBar>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <Link href="/">{t('home')}</Link>
+            <Link href="/about">{t('about')}</Link>
+            <Link href="/contact">{t('contact')}</Link>
+            <Link href="/signup">{t('signup')}</Link>
+            <LanguageDropdown />
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      {/* Empty toolbar to not have content behind the AppBar  */}
+      <Toolbar />
+    </header>
   );
 };
