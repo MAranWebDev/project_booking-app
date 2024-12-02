6c51d7c362f6623a076a97c931a86791fbd9b45d
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-02 17:33:00 -0300

    test

--- src/features/auth/components/signin-form.tsx
+++ src/features/auth/components/signin-form.tsx
@@ -3,11 +3,12 @@
 import { usersZodSignin, UsersZodSignin } from '@/libs/zod/schemas';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { signIn } from 'next-auth/react';
 import { useTranslations } from 'next-intl';
-
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 
@@ -36,25 +37,35 @@ export const SigninForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack spacing={2}>
-        <TextField
-          label="Email"
-          error={!!errors.email}
-          helperText={errors.email?.message}
-          {...register('email')}
-        />
-        <TextField
-          label={t('password')}
-          error={!!errors.password}
-          helperText={errors.password?.message}
-          {...register('password')}
-        />
+    <Paper sx={{ width: '450px', p: 6 }}>
+      <Image
+        style={{ margin: 'auto' }}
+        src="/images/logo.png"
+        alt="Logo"
+        width={150}
+        height={150}
+      />
 
-        <Button variant="contained" type="submit">
-          {t('signin')}
-        </Button>
-      </Stack>
-    </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={2}>
+          <TextField
+            label={t('email')}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+            {...register('email')}
+          />
+          <TextField
+            label={t('password')}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+            {...register('password')}
+          />
+
+          <Button variant="contained" type="submit">
+            {t('signin')}
+          </Button>
+        </Stack>
+      </form>
+    </Paper>
   );
 };
