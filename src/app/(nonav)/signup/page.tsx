3b5efccdaa0e99eb039fd6dca574e1715b5d9a72
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-06 18:03:27 -0300

    test

---
+++ src/app/(nonav)/signup/page.tsx
@@ -0,0 +1,19 @@
+import { SignupForm } from '@/features/auth/components';
+import Box from '@mui/material/Box';
+
+export default function SignupPage() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+      component="main"
+    >
+      <SignupForm />
+    </Box>
+  );
+}
--- src/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupForm } from '@/features/auth/components';
-
-export default function SignupPage() {
-  return (
-    <main>
-      <SignupForm />
-    </main>
-  );
-}
