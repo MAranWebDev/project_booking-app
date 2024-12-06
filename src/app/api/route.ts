99d7d4eba03d0e81b6292d46d2e1fcad0ebf7812
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-06 16:26:03 -0300

    format changes

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from 'next/server';
+
 export async function GET() {
-  return Response.json({ message: 'Hello API' });
+  return NextResponse.json({ message: 'Hello API' });
 }
