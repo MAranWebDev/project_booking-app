c698b8f3171c9b0fe554a2e03ef17ba8f318ea97
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-08 16:43:16 -0300

    request-config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
+// "next.config.ts" is not in "tsconfig" so it can't use absolute import
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 // Constants
-const REQUEST_PATH = './src/libs/next-intl/request.ts';
+const REQUEST_CONFIG_PATH = './src/libs/next-intl/request-config.ts';
 
 // "next-intl"
-const withNextIntl = createNextIntlPlugin(REQUEST_PATH);
+const withNextIntl = createNextIntlPlugin(REQUEST_CONFIG_PATH);
 
 const nextConfig: NextConfig = {
   /* config options here */
