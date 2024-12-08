512d241ec280f85c1a009bca3b5997b7bdc2394b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-12-08 10:53:55 -0300

    mui-x-date-pickers

--- src/libs/mui/provider/mui-x-date-pickers.provider.tsx
+++ src/libs/mui/provider/mui-x-date-pickers.provider.tsx
@@ -2,11 +2,20 @@
 
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/es-mx';
+import { useLocale } from 'next-intl';
 import { PropsWithChildren } from 'react';
 
 export const MuiXDatePickersProvider = ({ children }: PropsWithChildren) => {
+  // "next-intl"
+  const locale = useLocale();
+  const currentLocale = locale === 'es' ? locale : 'en';
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider
+      dateAdapter={AdapterDayjs}
+      adapterLocale={currentLocale}
+    >
       {children}
     </LocalizationProvider>
   );
